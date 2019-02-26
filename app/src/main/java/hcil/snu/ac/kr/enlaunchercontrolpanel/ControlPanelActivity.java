6a47d754c660406dafc0c8fac032d11e06017544
sharkmir1 <sharkmir1@snu.ac.kr>
2019-02-26 11:32:14 +0900

    initial commit

---
+++ app/src/main/java/hcil/snu/ac/kr/enlaunchercontrolpanel/ControlPanelActivity.java
@@ -0,0 +1,13 @@
+package hcil.snu.ac.kr.enlaunchercontrolpanel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControlPanelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controlpanel);
+    }
+}
