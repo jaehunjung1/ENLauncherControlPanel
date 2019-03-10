8f602df79241ca948314c9df4659fb3a0e31dea9
sharkmir1 <sharkmir1@snu.ac.kr>
2019-03-10 12:36:33 +0900

    aura preview on work...

---
+++ app/src/main/java/hcil/snu/ac/kr/enlaunchercontrolpanel/AuraPreview.kt
@@ -0,0 +1,17 @@
+package hcil.snu.ac.kr.enlaunchercontrolpanel
+
+import android.content.Context
+import android.view.View
+
+class AuraPreview(context: Context?) : View(context) {
+
+    companion object {
+        const val TAG ="AURA_PREVIEW"
+    }
+
+    init {
+    }
+
+    
+
+}
--- app/src/main/java/hcil/snu/ac/kr/enlaunchercontrolpanel/controlpanel/Setting1Fragment.java
+++ app/src/main/java/hcil/snu/ac/kr/enlaunchercontrolpanel/controlpanel/Setting1Fragment.java
@@ -1,9 +1,9 @@
 package hcil.snu.ac.kr.enlaunchercontrolpanel.controlpanel;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,8 +30,8 @@ public class Setting1Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_setting1, container, false);
+        ViewGroup parentLayout = (ViewGroup) inflater.inflate(R.layout.fragment_setting1, container, false);
+        return parentLayout;
     }
 
     @Override
