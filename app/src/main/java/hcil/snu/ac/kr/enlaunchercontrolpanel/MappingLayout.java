1c8b093037a44baf3297e75a427276b9545566ce
sharkmir1 <sharkmir1@snu.ac.kr>
2019-05-31 23:08:59 +0900

    끄앙

--- app/src/main/java/hcil/snu/ac/kr/enlaunchercontrolpanel/MappingLayout.java
+++ app/src/main/java/hcil/snu/ac/kr/enlaunchercontrolpanel/MappingLayout.java
@@ -1,21 +1,35 @@
 package hcil.snu.ac.kr.enlaunchercontrolpanel;
 
 import android.app.Activity;
-import android.support.constraint.ConstraintLayout;
+import android.content.DialogInterface;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.content.Context;
 import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.nex3z.flowlayout.FlowLayout;
+import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
+import com.pes.androidmaterialcolorpickerdialog.ColorPickerCallback;
+import com.robertlevonyan.views.chip.Chip;
+import com.robertlevonyan.views.chip.OnCloseClickListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import hcil.snu.ac.kr.enlaunchercontrolpanel.Utilities.Utilities;
 import hcil.snu.ac.kr.enlaunchercontrolpanel.ViewModel.MappingContainer;
 
 
@@ -44,7 +58,7 @@ public class MappingLayout extends LinearLayout {
     private Spinner notiPropSpinner;
 
     // Data Parameters
-    String visVar, notiProp; // selected variable & property
+    String visVar, notiProp = ""; // selected variable & property
 
 
     public MappingLayout(Context context, String visVar) {
@@ -67,10 +81,9 @@ public class MappingLayout extends LinearLayout {
                     notiProp = "";
                 } else {
                     notiProp = (String) adapterView.getItemAtPosition(i);
+                    showMappingDialog();
                 }
 
-                // TODO dialog function!!!
-                showMappingDialog();
 
             }
 
@@ -106,10 +119,16 @@ public class MappingLayout extends LinearLayout {
         final AlertDialog mDialog = mBuilder.create();
         mDialog.show();
 
-        // TODO set views for mapping dialog
-        if ()
+
+        // if both continuous, need to create the continuous graph
+        if (MappingContainer.isBothContinuous(visVar, notiProp)) {
+            // TODO
+        } else {
+            setMappingContent(dialogLayout);
+        }
 
         FrameLayout frameLayout = dialogLayout.findViewById(R.id.dialog_frame_layout);
+
 
 
 
@@ -131,6 +150,134 @@ public class MappingLayout extends LinearLayout {
 
     }
 
+    private void setMappingContent(ViewGroup dialogLayout) {
+        final LayoutInflater inflater = ((Activity)getContext()).getLayoutInflater();
+
+
+        ((FrameLayout)dialogLayout.findViewById(R.id.dialog_frame_layout)).addView(
+                inflater.inflate(R.layout.layout_nominal_mapping, (ViewGroup)getParent(), false)
+        );
+
+        TextView visVarTextView = dialogLayout.findViewById(R.id.vis_var_text_view);
+        TextView notiPropTextView = dialogLayout.findViewById(R.id.noti_property_text_view);
+        visVarTextView.setText(visVar);
+        notiPropTextView.setText(notiProp);
+
+        LinearLayout visVarDialogList = dialogLayout.findViewById(R.id.vis_var_dialog_list);
+        LinearLayout notiPropDialogList = dialogLayout.findViewById(R.id.noti_prop_dialog_list);
+
+        // setting visVarListView
+        if (visVar.equals("Motion")) {
+            for (int i = 0; i < visVarDialogList.getChildCount(); i++) {
+                final FrameLayout frame = (FrameLayout) visVarDialogList.getChildAt(i);
+                frame.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.rounded_rectangle));
+
+                // TODO for now, adding just dummy text view
+                TextView tv = new TextView(getContext());
+                tv.setText("Motion " + i);
+
+                frame.addView(tv);
+            }
+        } else if (visVar.equals("Shape")) {
+            for (int i = 0; i < visVarDialogList.getChildCount(); i++) {
+                final FrameLayout frame = (FrameLayout) visVarDialogList.getChildAt(i);
+                frame.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.rounded_rectangle));
+
+                // TODO for now, addign just dummy text view
+                TextView tv = new TextView(getContext());
+                tv.setText("Shape " + i);
+
+                frame.addView(tv);
+            }
+        } else {
+            for (int i = 0; i < visVarDialogList.getChildCount(); i++) {
+                final FrameLayout frame = (FrameLayout) visVarDialogList.getChildAt(i);
+
+                // TODO color 반영 될 수 있도록 변경
+                final int origColor = ContextCompat.getColor(getContext(), R.color.holo_blue_light);
+                frame.setBackgroundColor(origColor);
+
+                frame.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        final ColorPicker cp = new ColorPicker((Activity)getContext(),
+                                Color.red(origColor), Color.green(origColor), Color.blue(origColor)
+                        );
+                        cp.show();
+                        cp.enableAutoClose();
+                        cp.setCallback(new ColorPickerCallback() {
+                            @Override
+                            public void onColorChosen(int color) {
+                                frame.setBackgroundColor(color);
+                            }
+                        });
+                    }
+                });
+            }
+        }
+
+        // Setting Noti Property ListView
+        if (notiProp.equals("Keyword")) {
+            for (int i = 0; i < notiPropDialogList.getChildCount(); i++) {
+                final FrameLayout frame = (FrameLayout) notiPropDialogList.getChildAt(i);
+                frame.getLayoutParams().width = Utilities.dpToPx(getContext(), 170);
+
+                LinearLayout keywordFrame = (LinearLayout)inflater.inflate(
+                        R.layout.layout_keyword_mapping, (ViewGroup)getParent(), false
+                );
+                final FlowLayout flowLayout = keywordFrame.findViewById(R.id.mapping_keyword_flowLayout);
+                final ImageButton addButton = keywordFrame.findViewById(R.id.mapping_keyword_add_button);
+                addButton.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        final AlertDialog.Builder mBuilder = new AlertDialog.Builder(
+                                getContext(), android.R.style.Theme_Holo_Light_Dialog_NoActionBar
+                        );
+                        final EditText editText = new EditText(getContext());
+                        mBuilder.setView(editText);
+                        mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                addKeywordToFlowLayout(flowLayout, editText.getText().toString(), inflater);
+                            }
+                        });
+                        mBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                            }
+                        });
+
+                        final AlertDialog mDialog = mBuilder.create();
+                        mDialog.show();
+                    }
+                });
+
+
+                frame.addView(keywordFrame);
+
+            }
+        } else {
+            ArrayList<String> intervals = new ArrayList<>();
+            if (notiProp.equals("Interaction Stage")) {
+
+            } else {
+
+            }
+        }
+    }
+
+    private void addKeywordToFlowLayout(final FlowLayout flowLayout, String keyword, LayoutInflater inflater) {
+        final Chip chipView = (Chip)inflater.inflate(R.layout.chip_view_layout, null);
+        chipView.setChipText(keyword);
+        flowLayout.addView(chipView);
+        chipView.setOnCloseClickListener(new OnCloseClickListener() {
+            @Override
+            public void onCloseClick(View v) {
+                chipView.setVisibility(View.GONE);
+                ((ViewGroup)chipView.getParent()).removeView(chipView);
+            }
+        });
+    }
 
     private ArrayAdapter<String> getArrayAdapter(List<String> stringList) {
         return new ArrayAdapter<String>(getContext(), R.layout.spinner_item, stringList) {
--- app/src/main/java/hcil/snu/ac/kr/enlaunchercontrolpanel/ViewModel/MappingContainer.java
+++ app/src/main/java/hcil/snu/ac/kr/enlaunchercontrolpanel/ViewModel/MappingContainer.java
@@ -6,15 +6,25 @@ import java.util.List;
 
 public class MappingContainer {
     public static List<String> visVarStringList  = new ArrayList<>(
-            Arrays.asList("motion", "position", "shape", "size", "color")
+            Arrays.asList("Motion", "Position", "Shape", "Size", "Color")
     );
     public static List<String> notiPropStringList = new ArrayList<>(
             Arrays.asList("None", "Importance", "Interaction Stage", "Keyword")
     );
 
-    String visVar, notiProp;
-    boolean isVisVarNominal, isNotiPropNominal;
+    public static boolean isBothContinuous(String vis, String noti) {
+        return (!isNominal(vis)) && (!isNominal(noti));
+    }
 
+    public static boolean isNominal(String var) {
+        if (visVarStringList.contains(var)) {
+            return !(var.equals("Position") || var.equals("Size"));
+        } else {
+            return !(var.equals("Importance"));
+        }
+    }
+
+    String visVar, notiProp;
 
     public MappingContainer(String visVar, String notiProp) {
         this.visVar = visVar;
