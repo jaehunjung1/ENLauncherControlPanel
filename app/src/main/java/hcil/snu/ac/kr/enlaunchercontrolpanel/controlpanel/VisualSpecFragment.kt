1b78e02c659babb018c7858f840523422a175123
Jae Hun Jung <sharkmir1@snu.ac.kr>
2019-06-27 09:03:49 +0900

    changed mapping layout

--- app/src/main/java/hcil/snu/ac/kr/enlaunchercontrolpanel/controlpanel/VisualSpecFragment.kt
+++ app/src/main/java/hcil/snu/ac/kr/enlaunchercontrolpanel/controlpanel/VisualSpecFragment.kt
@@ -8,20 +8,13 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 
-import com.alespero.expandablecardview.ExpandableCardView
 
-import hcil.snu.ac.kr.enlaunchercontrolpanel.AggregatedMappingLayout
-import hcil.snu.ac.kr.enlaunchercontrolpanel.IndependentMappingLayout
 import hcil.snu.ac.kr.enlaunchercontrolpanel.R
 import hcil.snu.ac.kr.enlaunchercontrolpanel.recyclerviewmodel.HaloVisComponentAdapter
 import hcil.snu.ac.kr.enlaunchercontrolpanel.recyclerviewmodel.HaloVisComponent
-import hcil.snu.ac.kr.enlaunchercontrolpanel.viewmodel.MappingContainer
 import kr.ac.snu.hcil.datahalo.manager.AppHaloLayoutMethods
-import kr.ac.snu.hcil.datahalo.manager.VisEffectManager
 import kr.ac.snu.hcil.datahalo.ui.viewmodel.AppHaloConfigViewModel
-import kr.ac.snu.hcil.datahalo.visconfig.NuNotiVisVariable
 
 class VisualSpecFragment : Fragment() {
 
